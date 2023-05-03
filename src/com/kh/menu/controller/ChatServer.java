c67cad4e09587b7ab98f3d37328f5228a9bbd16a
CHOMINSU <tntntnfkd@hansei.ac.kr>
2023-05-03 20:43:54 +0900

    채팅방 수정
    채팅방 수정

--- src/com/kh/menu/controller/ChatServer.java
+++ src/com/kh/menu/controller/ChatServer.java
@@ -5,8 +5,6 @@ import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 
-import javax.servlet.http.HttpSession;
-import javax.websocket.EndpointConfig;
 import javax.websocket.OnClose;
 import javax.websocket.OnError;
 import javax.websocket.OnMessage;
@@ -16,27 +14,28 @@ import javax.websocket.server.ServerEndpoint;
 
 import com.kh.member.model.vo.Member;
 
+
 @ServerEndpoint("/chat")
 public class ChatServer {
     private static Set<Session> clients = Collections.synchronizedSet(new HashSet<>());
-    
- 
-    
-    @OnOpen 
-    public void onConnect(Session session, EndpointConfig config) { 
-    	HttpSession hSession = (HttpSession)config.getUserProperties().get("hSession"); 
-    	clients.add(session);
-    	System.out.println(hSession.getAttribute("loginID"));
+
+    @OnOpen
+    public void onOpen(Session session) {
+        System.out.println("WebSocket opened: " + session.getId());
+        session.getUserProperties().put("userId", ""); // 빈 문자열로 초기화
+        clients.add(session);
     }
 
     @OnMessage
     public void onMessage(String message, Session session) {
         System.out.println("Message received: " + message);
 
-        for (Session client : getAllClients()) {
+        // 모든 클라이언트에게 메시지 전송
+        for (Session client : session.getOpenSessions()) {
             try {
-                String userId = (String) session.getUserProperties().get("userId");
-                String fullMessage = userId + " " + message;
+                // 클라이언트로 보낼 메시지 작성 (보낸 사용자 ID + 메시지 내용)
+            	String userId = (String) session.getUserProperties().get("userId");
+            	String fullMessage = userId + " " + message;
 
                 client.getBasicRemote().sendText(fullMessage);
             } catch (IOException e) {
@@ -54,9 +53,5 @@ public class ChatServer {
     @OnError
     public void onError(Throwable error) {
         error.printStackTrace();
-    }
-
-    private Set<Session> getAllClients() {
-        return clients;
     }
 }
